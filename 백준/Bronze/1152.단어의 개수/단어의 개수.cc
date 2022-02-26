6ad11518a4929ab130220088f592dba917a29085
lClarified <73734127+lClarified@users.noreply.github.com>
2022-02-26 11:48:34 +0900

    [Bronze II] Title: 단어의 개수, Time: 44 ms, Memory: 4876 KB -BaekjoonHub

---
+++ 백준/Bronze/1152.단어의 개수/단어의 개수.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int cnt=0;
+    string s;
+
+    while(cin>>s){
+        cnt+=1;
+    }
+
+    cout<<cnt<<'\n';
+    return 0;
+}
