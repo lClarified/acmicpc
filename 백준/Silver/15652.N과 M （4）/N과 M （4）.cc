6c94c23df6b5cc4bc36ee5d3a437e3e909a39095
lClarified <73734127+lClarified@users.noreply.github.com>
2022-02-26 10:41:28 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652.N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N,M,check[9];
+vector<int> vec;
+int b=1;
+
+
+void f(int a){
+    if(a == M){
+        for(int i=0;i<M;i++){
+            cout<<vec[i]<<" ";
+        }
+        cout << "\n";
+        return;
+    }
+    for(int i=b;i<=N;i++){
+        if(check[i]==0){
+            vec.push_back(i);
+            b=i;
+            f(a+1);
+            check[i] = 0;
+            vec.pop_back();
+        }
+    }
+}
+
+
+int main()
+{
+    cin>>N>>M;
+    f(0);
+
+
+    return 0;
+}
