4ef3bc415c9105ff1846646c319e640fc9529c96
lClarified <73734127+lClarified@users.noreply.github.com>
2022-03-03 08:40:43 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+int dp[1000001];
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    cin>>N;
+    dp[1]=1;
+    dp[2]=2;
+    for(int i=3;i<=N;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+        dp[i]%=15746;
+    }
+
+    cout<<dp[N];
+
+
+    return 0;
+}
+
