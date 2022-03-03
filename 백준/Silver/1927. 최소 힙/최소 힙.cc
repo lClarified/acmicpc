d5727540dee72d02a34f25f8b979c1e4c3debb1b
lClarified <73734127+lClarified@users.noreply.github.com>
2022-03-03 09:10:45 +0900

    [Silver II] Title: 최소 힙, Time: 68 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+priority_queue<int, vector<int>, greater<int>> qu;
+
+
+
+int main()
+{
+    cin.tie(0);
+    cout.tie(0);
+    cin>>N;
+    for(int i=0;i<N;i++){
+        int x;
+        cin>>x;
+        if(x==0&&qu.empty()){
+            cout<<"0"<<'\n';
+        }
+        else if(x==0&&!qu.empty()){
+            cout<<qu.top()<<'\n';
+            qu.pop();
+        }
+        else
+            qu.push(x);
+    }
+
+
+    return 0;
+}
