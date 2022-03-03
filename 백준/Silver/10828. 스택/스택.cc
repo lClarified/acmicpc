20d8dc1d751589147a5df37b3400b8413a0efcd1
lClarified <73734127+lClarified@users.noreply.github.com>
2022-03-03 09:35:08 +0900

    [Silver IV] Title: 스택, Time: 312 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    stack<int> s;
+
+    while (n--){
+        string cmd;
+        cin>>cmd;
+        if(cmd =="push"){
+            int num;
+            cin>>num;
+            s.push(num);
+
+        }
+        else if(cmd=="top"){
+            cout<<(s.empty()?-1:s.top())<<'\n';
+        }
+        else if(cmd=="size"){
+            cout<<(s.size())<<'\n';
+        }
+        else if(cmd=="pop"){
+            cout<<(s.empty()?-1:s.top())<<'\n';
+            if(!s.empty()){
+                s.pop();
+            }
+        }
+        else if(cmd=="empty"){
+            cout<<(s.empty())<<'\n';
+        }
+
+    }
+    return 0;
+}
