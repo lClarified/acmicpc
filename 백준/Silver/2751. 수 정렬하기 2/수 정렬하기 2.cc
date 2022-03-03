35100c0c4fc7837815290575fc325ffb19955d90
lClarified <73734127+lClarified@users.noreply.github.com>
2022-03-03 09:23:52 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 672 ms, Memory: 5892 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+int a[1000001];
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)cin>>a[i];
+
+    sort(a,a+n);
+
+    for(int i=0;i<n;i++) cout<<a[i]<<'\n';
+    return 0;
+}
