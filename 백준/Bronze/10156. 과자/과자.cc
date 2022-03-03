afea5083911055a9b539e4b981e0be7dd203a34e
lClarified <73734127+lClarified@users.noreply.github.com>
2022-03-03 09:31:24 +0900

    [Bronze IV] Title: 과자, Time: 0 ms, Memory: 1980 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int k,n,m;
+    cin>>k>>n>>m;
+    if(k*n-m<0)
+        cout<<"0";
+    else
+        cout<<k*n-m;
+    return 0;
+}
