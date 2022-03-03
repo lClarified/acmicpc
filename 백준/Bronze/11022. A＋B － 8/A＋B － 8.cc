8898564e14f2794e96d49000187f3e883888de18
lClarified <73734127+lClarified@users.noreply.github.com>
2022-03-03 09:48:05 +0900

    [Bronze III] Title: A+B - 8, Time: 4 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+
+using namespace std;
+
+int main(void)
+{
+
+    int n,a,b;
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+        cin>>a>>b;
+        printf("Case #%d: %d + %d = %d\n",i,a,b,a+b);
+    }
+	return 0;
+}
