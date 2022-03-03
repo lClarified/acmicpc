00133877d205f1067dd9fe401d59bf00e0298883
lClarified <73734127+lClarified@users.noreply.github.com>
2022-03-03 09:29:18 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int j,n,a;
+    a=0;
+    scanf("%d",&n);
+    for(j=1;j<=n;j++)
+        a+=j;
+    printf("%d",a);
+
+
+
+    return 0;
+}
